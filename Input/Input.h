3cb24cafff6482a4606ed1264dfe83f519e1cc88
warias <warias@tuenti.com>
2018-02-05 22:50:11 +0100

    creating defineCommand and command macros to simplify command definitions

--- Input/Input.h
+++ Input/Input.h
@@ -1,6 +1,9 @@
 #ifndef INPUT_COMMAND_READER_H
 #define INPUT_COMMAND_READER_H
 
+#define defineCommands(...)  {__VA_ARGS__,NULL}
+#define command(X,Y,Z)  new InputCommand(X, Y, Z)
+
 #include "InputCommand.h"
 
 #define INPUT_COMMAND_MAX_PARAMS 3
